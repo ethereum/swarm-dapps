6b1bcfb05663bc421071c973cfada02aedcb526d
Sergii Bomko <aquila.ua@gmail.com>
2018-12-23 14:05:24 +0100

    revert proxy runner

--- start-proxy.js
+++ start-proxy.js
@@ -8,8 +8,8 @@ if ('node' === path.basename(args[0])) {
     args = args.slice(1);
 }
 
-var dir = path.resolve(args[2]);
-console.log("Root:", dir)
+var dir = path.resolve(args[1]);
+
 
 var params = {
     port: 8888, // Set the server port. Defaults to 8080.
@@ -20,11 +20,11 @@ var params = {
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
     logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
     proxy: [
-        ['/bzz:', 'http://localhost:32200/bzz:'],
-        ['/bzzr:', 'http://localhost:32200/bzzr:'],
-        ['/bzzi:', 'http://localhost:32200/bzzi:'],
+        [ '/bzz:', 'http://localhost:32200/bzz:' ],
+        [ '/bzzr:', 'http://localhost:32200/bzzr:' ],
+        [ '/bzzi:', 'http://localhost:32200/bzzi:' ],
     ],
-    middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
 };
 
 liveServer.start(params);
